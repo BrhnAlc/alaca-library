696f185f51e931bb36414b0bbb23c3359e8f8b6d
burhan <alacaburhan122016@gmail.com>
2023-09-13 01:20:44 +0300

    about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -5,22 +5,22 @@ import {
   InfoContainer,
   ProfileImg,
 } from "./About.style";
-import profile from "../../assets/about.png";
+import profile from "../../assets/about.jpg";
 import Icon from "../../components/icon/Icon";
 import { iconData } from "../../helper/iconData";
 
 const About = () => {
   return (
     <AboutContainer>
-      <ProfileImg src={profile} alt="anthony" />
+      <ProfileImg src={profile} alt="alaca" />
       <InfoContainer>
-        <h2>Hi, I'm Anthony</h2>
+        <h2>Hi, I'm alaca</h2>
         <h3>
           I'm currently learning different full-stack development languages.
         </h3>
         <h4>
-          I already know JavaScript, ReactJS, Next.js, React Native, Django,
-          Node.js, MongoDB, SQL, Python, and AWS services.
+          I already know JavaScript, ReactJS, Next.js, React Native, 
+          Node.js and  Python.
         </h4>
         <h4>💬 You can ask me anything you want to know</h4>
         <IconContainer>
